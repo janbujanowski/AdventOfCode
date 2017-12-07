318fc6cc4f5894a688e8776c59552e77221e9f44
jan.bujanowski <jan.bujanowski@gmail.com>
2017-12-07 10:14:34 +0100

    first code - need serious restructuring

---
+++ Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+
+namespace AdventOfCode
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Day2.Program.RunDay2();
+            
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
