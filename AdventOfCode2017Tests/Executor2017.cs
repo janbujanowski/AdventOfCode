c73191e293d9d8140671654cf04790bae8a6e295
jan.bujanowski@g1ant.com <jan.bujanowski@g1ant.com>
2018-01-02 17:36:15 +0100

    day 12 prepare

--- AdventOfCode2017Tests/Executor2017.cs
+++ AdventOfCode2017Tests/Executor2017.cs
@@ -486,6 +486,48 @@ namespace AdventOfCode2017Tests
             Assert.AreEqual(1356, Day11.StarTwo(null));
         }
         [TestMethod]
+        public void Day12StarOne()
+        {
+            //---Day 12: Digital Plumber ---
+            //Walking along the memory banks of the stream, you find a small village that is experiencing a little confusion: some programs can't communicate with each other.
+
+            //Programs in this village communicate using a fixed system of pipes.Messages are passed between programs using these pipes, but most programs aren't connected to each other directly. Instead, programs pass messages between each other until the message reaches the intended recipient.
+
+            //For some reason, though, some of these messages aren't ever reaching their intended recipient, and the programs suspect that some pipes are missing. They would like you to investigate.
+
+            //You walk through the village and record the ID of each program and the IDs with which it can communicate directly(your puzzle input).Each program has one or more programs with which it can communicate, and these pipes are bidirectional; if 8 says it can communicate with 11, then 11 will say it can communicate with 8.
+
+            //You need to figure out how many programs are in the group that contains program ID 0.
+
+            //For example, suppose you go door - to - door like a travelling salesman and record the following list:
+
+            //            0 <-> 2
+            //1 <-> 1
+            //2 <-> 0, 3, 4
+            //3 <-> 2, 4
+            //4 <-> 2, 3, 6
+            //5 <-> 6
+            //6 <-> 4, 5
+            //In this example, the following programs are in the group that contains program ID 0:
+
+            //Program 0 by definition.
+            //Program 2, directly connected to program 0.
+            //Program 3 via program 2.
+            //Program 4 via program 2.
+            //Program 5 via programs 6, then 4, then 2.
+            //Program 6 via programs 4, then 2.
+            //Therefore, a total of 6 programs are in this group; all but program 1, which has a pipe that connects it to itself.
+
+            //How many programs are in the group that contains program ID 0 ?
+            Assert.AreEqual(1034, Day12.StarOne());
+        }
+        [TestMethod]
+        public void Day12StarTwo()
+        {
+
+            Assert.AreEqual(1356, Day12.StarTwo());
+        }
+        [TestMethod]
         public void Day18StarOne()
         {
             //You discover a tablet containing some strange assembly code labeled simply "Duet".Rather than bother the sound card with it, you decide to run the code yourself.Unfortunately, you don't see any documentation, so you're left to figure out what the instructions mean on your own.
