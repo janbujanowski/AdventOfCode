f8504b9d7d6836bdebeccabf597d646c615f8fdb
jan.bujanowski@g1ant.com <jan.bujanowski@g1ant.com>
2018-01-19 15:36:08 +0100

    fixed and reworked day11 - might need fixes for some cases

--- AdventOfCode2017Tests/Executor2017.cs
+++ AdventOfCode2017Tests/Executor2017.cs
@@ -476,14 +476,36 @@ namespace AdventOfCode2017Tests
         [TestMethod]
         public void Day11StarOne()
         {
+            //Crossing the bridge, you've barely reached the other side of the stream when a program comes up to you, clearly in distress. "It's my child process," she says, "he's gotten lost in an infinite grid!"
 
+            //Fortunately for her, you have plenty of experience with infinite grids.
+
+            //Unfortunately for you, it's a hex grid.
+
+            //The hexagons("hexes") in this grid are aligned such that adjacent hexes can be found to the north, northeast, southeast, south, southwest, and northwest:
+
+            //  \ n /
+            //nw + --+ne
+            //  /    \
+            //-+ +-
+            //  \    /
+            //sw + --+se
+            //  / s  \
+            //You have the path the child process took.Starting where he started, you need to determine the fewest number of steps required to reach him. (A "step" means to move from the hex you are in to any adjacent hex.)
+
+            //For example:
+
+            //ne, ne, ne is 3 steps away.
+            //ne, ne, sw, sw is 0 steps away(back where you started).
+            //ne, ne, s, s is 2 steps away(se, se).
+            //se, sw, se, sw, sw is 3 steps away(s, s, sw).
             Assert.AreEqual(796, Day11.StarOne(null));
         }
         [TestMethod]
         public void Day11StarTwo()
         {
-            //1872 too high
-            Assert.AreEqual(1356, Day11.StarTwo(null));
+            //How many steps away is the **furthest** he ever got from his starting position?
+            Assert.AreEqual(1585, Day11.StarTwo(null));
         }
         [TestMethod]
         public void Day12StarOne()
