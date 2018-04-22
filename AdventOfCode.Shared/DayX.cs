a1c297b01c70cb9250ac61ceb02f214c75571cd8
yeti <jan.bujanowski@gmail.com>
2018-04-22 23:49:25 +0200

    omfg visual studio so stupid placing files in bad folders

---
+++ AdventOfCode.Shared/DayX.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode.Shared
+{
+    public abstract class DayX
+    {
+        public int DayNumber = -1;
+
+        public object StarOne()
+        {
+            return -1;
+        }
+        public object StarTwo()
+        {
+            return -1;
+        }
+    }
+}
