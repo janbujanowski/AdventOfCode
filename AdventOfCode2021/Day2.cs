b26e1fe419a92bed20a71d656c7ea62463f61cd4
CHILL\bot <jan.bujanowski@gmail.com>
2021-12-03 00:40:18 +0100

    day 2 OOP approach

--- AdventOfCode2021/Day2.cs
+++ AdventOfCode2021/Day2.cs
@@ -5,67 +5,101 @@ using System.Linq;
 
 namespace AdventOfCode2021
 {
-    public class Day2 : Day66
+    public class SubmarineStarOne
     {
         string[] lines;
-        int[] amountOfMovement;
-        string[] directionOfMovement;
-        public override void ParseInput(string strInput)
+        protected int[] amountOfMovement;
+        protected string[] directionOfMovement;
+        protected int horizontalPos;
+        protected int depth;
+        public SubmarineStarOne(string strInput)
         {
+            horizontalPos = 0; depth = 0;
             lines = strInput.Split("\r\n");
             amountOfMovement = lines.Select(line => int.Parse(line.Split(' ')[1])).ToArray();
             directionOfMovement = lines.Select(line => line.Split(' ')[0]).ToArray();
         }
+        public void Move()
+        {
+            for (int i = 0; i < amountOfMovement.Length; i++)
+            {
+                switch (directionOfMovement[i])
+                {
+                    case "forward":
+                        MoveForward(i);
+                        break;
+                    case "up":
+                        MoveUp(i);
+                        break;
+                    case "down":
+                        MoveDown(i);
+                        break;
+                    default:
+                        break;
+                }
+            }
+        }
+        public virtual void MoveForward(int step)
+        {
+            horizontalPos += amountOfMovement[step];
+        }
+        public virtual void MoveUp(int step)
+        {
+            depth -= amountOfMovement[step];
+        }
+        public virtual void MoveDown(int step)
+        {
+            depth += amountOfMovement[step];
+        }
+
+        public int GetStarResult()
+        {
+            return horizontalPos * depth;
+        }
+    }
+
+    public class SubmarineStarTwo : SubmarineStarOne
+    {
+        int aim;
+        public SubmarineStarTwo(string strInput):base(strInput)
+        {
+            aim = 0;
+        }
+        public override void MoveForward(int step)
+        {
+            horizontalPos += amountOfMovement[step];
+            depth += aim * amountOfMovement[step];
+        }
+        public override void MoveUp(int step)
+        {
+            aim -= amountOfMovement[step];
+        }
+        public override void MoveDown(int step)
+        {
+            aim += amountOfMovement[step];
+        }
+    }
+
+    public class Day2 : Day66
+    {
+
+        string _strInput;
+        public override void ParseInput(string strInput)
+        {
+            _strInput = strInput;
+        }
 
         public override object StarOne()
         {
-            int horizontalPos = 0;
-            int depth = 0;
-            for (int i = 0; i < amountOfMovement.Length; i++)
-            {
-                switch (directionOfMovement[i])
-                {
-                    case "forward":
-                        horizontalPos += amountOfMovement[i];
-                        break;
-                    case "up":
-                        depth -= amountOfMovement[i];
-                        break;
-                    case "down":
-                        depth += amountOfMovement[i];
-                        break;
-                    default:
-                        break;
-                }
-            }
-            return horizontalPos * depth;
+            SubmarineStarOne sub = new SubmarineStarOne(_strInput);
+            sub.Move();
+            return sub.GetStarResult();
         }
         public override object StarTwo()
         {
-            int horizontalPos = 0;
-            int depth = 0;
-            int aim = 0;
-            for (int i = 0; i < amountOfMovement.Length; i++)
-            {
-                switch (directionOfMovement[i])
-                {
-                    case "forward":
-                        horizontalPos += amountOfMovement[i];
-                        depth += amountOfMovement[i] * aim;
-                        break;
-                    case "up":
-                        //depth -= amountOfMovement[i];
-                        aim -= amountOfMovement[i];
-                        break;
-                    case "down":
-                        //depth += amountOfMovement[i];
-                        aim += amountOfMovement[i];
-                        break;
-                    default:
-                        break;
-                }
-            }
-            return horizontalPos * depth;
+            SubmarineStarTwo sub = new SubmarineStarTwo(_strInput);
+            sub.Move();
+            return sub.GetStarResult();
         }
     }
 }
