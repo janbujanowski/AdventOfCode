3eca8aaca34682a52aefc441707944d12c85c692
CHILL\bot <jan.bujanowski@gmail.com>
2021-12-08 15:23:35 +0100

    day 8 two stars omg not sure if optimal

--- AdventOfCode2021/Day8.cs
+++ AdventOfCode2021/Day8.cs
@@ -6,6 +6,31 @@ using System.Linq;
 
 namespace AdventOfCode2021
 {
+    public static class Day8Extensions
+    {
+        public static string Without(this string source, string toRemove)
+        {
+            var newString = source.ToList();
+            foreach (var letter in toRemove)
+            {
+                newString.Remove(letter);
+            }
+            return String.Concat(newString);
+        }
+        public static bool ContainsNumber(this string source, string toCheck)
+        {
+            int matchCount = 0;
+            var newString = source.ToList();
+            foreach (var letter in toCheck)
+            {
+                if (source.Contains(letter))
+                {
+                    matchCount++;
+                }
+            }
+            return toCheck.Length == matchCount;
+        }
+    }
     public class Day8 : Day66
     {
         Dictionary<int, int> amountOfsubmarinesOnPositionKey;
@@ -16,6 +41,8 @@ namespace AdventOfCode2021
 
         int[,] _signalInputsExamples;
         int[,] _signalOutputs;
+        Dictionary<int, string[]> _leftSide;
+        Dictionary<int, string[]> _rightSide;
         public override void ParseInput(string strInput)
         {
             _strInput = strInput;
@@ -24,68 +51,96 @@ namespace AdventOfCode2021
             _linesCount = lines.Length;
             _signalInputsExamples = new int[lines.Length, 10];
             _signalOutputs = new int[lines.Length, 4];
+            _leftSide = new Dictionary<int, string[]>();
+            _rightSide = new Dictionary<int, string[]>();
             for (int i = 0; i < lines.Length; i++)
             {
                 var line = lines[i].Split(" | ");
-                var inputs = line[0].Split(" ").Select(x => x.Count()).ToArray();
-                for (int j = 0; j < inputs.Length; j++)
-                {
-                    _signalInputsExamples[i, j] = inputs[j];
-                }
-                inputs = line[1].Split(" ").Select(x => x.Count()).ToArray();
-                for (int j = 0; j < inputs.Length; j++)
-                {
-                    _signalOutputs[i, j] = inputs[j];
-                }
+                var leftRow = line[0].Split(" ").Select(number => String.Concat(number.OrderBy(letter => (int)letter))).ToArray();
+                var rightRow = line[1].Split(" ").Select(number => String.Concat(number.OrderBy(letter => (int)letter))).ToArray();
+                //.Select(numbAsString => numbAsString.OrderBy(letter => (int)letter)
+                //order for easier debugging
+                _leftSide.Add(i, leftRow);
+                _rightSide.Add(i, rightRow);
             }
         }
 
         public override object StarOne()
         {
-            List<int> oneFourSevenEight = new List<int>() { 2, 3, 4, 7 };
             int sum = 0;
-            
-            for (int i = 0; i < _linesCount; i++)
+            for (int i = 0; i < _leftSide.Count; i++)
             {
-                for (int h = 0; h < 4; h++)
-                {
-                    if (oneFourSevenEight.Contains(_signalOutputs[i,h]))
-                    {
-                        sum++;
-                    }
-                }
+                Dictionary<string, int> numbersDefinitions = DefineNumbers(_leftSide[i].ToList());
+                int rightNumber = GetRightNumber(_rightSide[i], numbersDefinitions);
+                sum += rightNumber;
             }
 
             return sum;
         }
-        private int CalculateSingleSubConsumptionStarOne(int from, int to)
+
+        private int GetRightNumber(string[] vs, Dictionary<string, int> numbersDefinitions)
         {
-            return Math.Abs(from - to);
-        }
-        private uint CalculateAllSubsDistanceTo(int positionWithMaximumAmountOfSubs, Func<int, int, int> fuelConsumptionFunction)
-        {
-            uint fuel = 0;
-            int targetPostion = positionWithMaximumAmountOfSubs;
-            foreach (var item in amountOfsubmarinesOnPositionKey)
+            int modifier = 1000;
+            int sum = 0;
+            for (int i = 0; i < vs.Length; i++)
             {
-                if (item.Key != targetPostion)
-                {
-                    fuel += Convert.ToUInt32(fuelConsumptionFunction(item.Key, targetPostion) * item.Value);
-                }
+                sum += numbersDefinitions[vs[i]] * modifier;
+                modifier = modifier / 10;
             }
-            return fuel;
+            return sum;
         }
-        private int CalculateSingleSubConsumptionStarTwo(int from, int to)
+
+        private Dictionary<string, int> DefineNumbers(List<string> vs)
         {
-            int fuelConsumptionPerSub = 0;
-            for (int i = 1; i <= Math.Abs(from - to); i++)
+            //identyify 1 4 7 8 by size
+            string one = vs.Where(x => x.Length == 2).First();
+            string four = vs.Where(x => x.Length == 4).First();
+            string seven = vs.Where(x => x.Length == 3).First();
+            string eight = vs.Where(x => x.Length == 7).First();
+            vs.Remove(one);
+            vs.Remove(four);
+            vs.Remove(seven);
+            vs.Remove(eight);
+            //9 must contain 4
+            string nine = vs.Where(numb => numb.ContainsNumber(four) && numb.Length == 6).FirstOrDefault();
+            vs.Remove(nine);
+
+            //6 same size as 9 but cointain 4 without 1
+            string fourWithoutOne = four.Without(one);
+            var trySix = vs.Where(numb => numb.Intersect(fourWithoutOne).Count() == 2 && numb.Length == nine.Length);
+            string six = trySix.FirstOrDefault();
+            vs.Remove(six);
+            //0 same size as 9 but dont have full 4
+            string zero = vs.Where(numb => numb.Length == nine.Length).FirstOrDefault();
+            //5 must contain 4 without 1
+            string five = vs.Where(numb => numb.Length == 5 && numb.ContainsNumber(fourWithoutOne)).FirstOrDefault();
+            vs.Remove(five);
+            //3 must contain 1
+            string three = vs.Where(numb => numb.Length == 5 && numb.ContainsNumber(one)).FirstOrDefault();
+            //2 remaining size 5
+            vs.Remove(three);
+            string two = vs.Where(numb => numb.Length == 5).FirstOrDefault();
+
+            return new Dictionary<string, int>()
             {
-                fuelConsumptionPerSub += i;
-            }
-            return fuelConsumptionPerSub;
+                { zero , 0 },
+                { one , 1 },
+                { two , 2 },
+                { three , 3 },
+                { four , 4 },
+                { five , 5 },
+                { six , 6 },
+                { seven , 7 },
+                { eight , 8 },
+                { nine , 9 }
+            };
         }
+        private string[] _workingDefinitions;
+
         public override object StarTwo()
         {
+
+
             var res = -1;
             //var a = CalculateAllSubsDistanceTo(_meanLower, CalculateSingleSubConsumptionStarTwo);
             //var b = CalculateAllSubsDistanceTo(_meanUpper, CalculateSingleSubConsumptionStarTwo);
