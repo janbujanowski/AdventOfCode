46647012fc6e4505008df4b2cc1ff3c96d39489e
CHILL\bot <jan.bujanowski@gmail.com>
2021-12-12 18:25:18 +0100

    sth wrong with recurrency but close to done 11 star 1

--- AdventOfCode2021/Day11.cs
+++ AdventOfCode2021/Day11.cs
@@ -1,40 +1,136 @@
 ﻿using AdventOfCode.Shared;
 using AdventOfCode.Shared.Extensions;
 using System;
-using System.Collections.Generic;
-using System.Linq;
 
 namespace AdventOfCode2021
 {
-
     public class Day11 : Day66
     {
         string _strInput;
-        Dictionary<char, int> _lineStack;
-
-        List<char> _pushingChars = new List<char> { '{', '[', '<', '(' };
-        List<char> _poopingChars = new List<char> { '}', ']', '>', ')' };
-        Dictionary<char, char> _poopingDick = new Dictionary<char, char>() { { '}', '{' }, { ']', '[' }, { '>', '<' }, { ')', '(' } };
-        Dictionary<char, char> _pushingDick = new Dictionary<char, char>() { { '{', '}' }, { '[', ']' }, { '<', '>' }, { '(', ')' } };
-        Dictionary<char, int> _charValues = new Dictionary<char, int>()
-        { { ')', 3 }, { ']', 57 }, { '}', 1197 }, { '>', 25137 },
-          { '(', 1 }, { '[', 2 }, { '{', 3 }, { '<', 4 }
-        };
         string[] _lines;
-
-        Dictionary<int, char> _firstInvalidCharInLine;
-
-        Dictionary<int, string> _incompleteLinesClosingSequence;
+        int[,] dumboOctopusesMap;
         public override void ParseInput(string strInput)
         {
             _strInput = strInput;
             _lines = _strInput.Split("\r\n");
         }
+        private int[,] GenerateCleanMap()
+        {
+            int dimension = _lines.Length;
+            int[,] map = new int[dimension, dimension];
+            for (int y = 0; y < dimension; y++)
+            {
+                string line = _lines[y];
+                for (int x = 0; x < dimension; x++)
+                {
+                    map[y, x] = int.Parse(line[x].ToString());
+                }
+            }
+            return map;
+        }
+        int _flashes;
         public override object StarOne()
         {
+            dumboOctopusesMap = GenerateCleanMap();
+            int stepsCount = 3;
+            _flashes = 0;
+            for (int i = 0; i < stepsCount; i++)
+            {
+                ExecuteLifeCycle();
+                ExecuteHighlighteffect();
+                _flashes += CountFlashes();
+                ClearFlashed();
+                PrintDumbos();
+            }
+            return _flashes;
+        }
+        private void ExecuteLifeCycle()
+        {
+            for (int y = 0; y < dumboOctopusesMap.GetLength(0); y++)
+            {
+                for (int x = 0; x < dumboOctopusesMap.GetLength(1); x++)
+                {
+                    dumboOctopusesMap[y, x]++;
+                }
+            }
+        }
+
+        private void ExecuteHighlighteffect()
+        {
+            for (int y = 0; y < dumboOctopusesMap.GetLength(0); y++)
+            {
+                for (int x = 0; x < dumboOctopusesMap.GetLength(1); x++)
+                {
+                    if (dumboOctopusesMap[y, x] > 9)
+                    {
+                        BumpSurroundings(y, x);
+                    }
+                }
+            }
+        }
+        private void BumpSurroundings(int Ypos, int Xpos)
+        {
+            int starty = Ypos;
+            int startx = Xpos;
+            int endy = Ypos;
+            int endx = Xpos;
+            if (Ypos > 0)
+                starty--;
+            if (Ypos < dumboOctopusesMap.GetLength(0) - 1)
+                endy++;
+            if (Xpos > 0)
+                startx--;
+            if (Xpos < dumboOctopusesMap.GetLength(1) - 1)
+                endx++;
+            for (int y = starty; y <= endy; y++)
+            {
+                for (int x = startx; x <= endx; x++)
+                {
+                    if (dumboOctopusesMap[y, x] < 10)
+                    {
+                        dumboOctopusesMap[y, x]++;
+                    }
+                }
+            }
+        }
+        private void ClearFlashed()
+        {
+            for (int y = 0; y < dumboOctopusesMap.GetLength(0); y++)
+            {
+                for (int x = 0; x < dumboOctopusesMap.GetLength(1); x++)
+                {
+                    if (dumboOctopusesMap[y, x] > 9)
+                    {
+                        dumboOctopusesMap[y, x] = 0;
+                    }
+                }
+            }
+        }
+
+        private int CountFlashes()
+        {
             int sum = 0;
-         
+            foreach (var number in dumboOctopusesMap)
+            {
+                if (number > 9)
+                {
+                    sum++;
+                }
+            }
             return sum;
+        }
+
+        private void PrintDumbos()
+        {
+            for (int y = 0; y < dumboOctopusesMap.GetLength(0); y++)
+            {
+                for (int x = 0; x < dumboOctopusesMap.GetLength(1); x++)
+                {
+                    Console.Write(dumboOctopusesMap[y, x]);
+                }
+                Console.WriteLine();
+            }
+            Console.WriteLine();
         }
         public override object StarTwo()
         {
