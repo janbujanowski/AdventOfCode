e5fbf8ff776901ff91c2e8d030b7de67d8a37de7
CHILL\bot <jan.bujanowski@gmail.com>
2021-12-12 19:34:22 +0100

    day 12 clean setup

---
+++ AdventOfCode2021/Day12.cs
@@ -0,0 +1,29 @@
+﻿using AdventOfCode.Shared;
+using AdventOfCode.Shared.Extensions;
+
+namespace AdventOfCode2021
+{
+    public class Day12 : Day66
+    {
+        string _strInput;
+        string[] _lines;
+        int _flashes;
+        public override void ParseInput(string strInput)
+        {
+            _strInput = strInput;
+            _lines = _strInput.Split("\r\n");
+        }
+
+        public override object StarOne()
+        {
+            _flashes = 0;
+            return _flashes;
+        }
+
+        public override object StarTwo()
+        {
+            int stepsCount = 0;
+            return stepsCount;
+        }
+    }
+}
--- Program.cs
+++ Program.cs
@@ -35,7 +35,7 @@ namespace AdventOfCodeRunner
 
             //Execution
             int year = 2021;
-            int day = 11;
+            int day = 12;
 
             var workingDay = solutionContainer.First(riddleSolution => riddleSolution.DayNumber == day && riddleSolution.YearNumber == year);
             if (workingDay != null)
