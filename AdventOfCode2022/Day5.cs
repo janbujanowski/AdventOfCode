5b9685449647a28bf99cbb3c840dd662f514ab62
janbujanowski <jan.bujanowski@gmail.com>
2022-12-05 04:09:25 +0100

    prepare empty day 5

---
+++ AdventOfCode2022/Day5.cs
@@ -0,0 +1,34 @@
+﻿using AdventOfCode.Shared;
+using AdventOfCode.Shared.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace AdventOfCode2022
+{
+    public class Day5 : Day66
+    {
+        string _strInput;
+        string[] _lines;
+        public override void ParseInput(string strInput)
+        {
+            _strInput = strInput;
+            _lines = _strInput.Split("\r\n");
+        }
+
+        public override object StarOne()
+        {
+            int sum = 0;
+            return sum;
+        }
+       
+        public override object StarTwo()
+        {
+            int sum = 0;
+            return sum;
+        }
+        
+    }
+}
