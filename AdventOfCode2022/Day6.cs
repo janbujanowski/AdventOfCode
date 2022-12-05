15184c70b7e6912e723e2e056a5d91e1f1f3d11f
janbujanowski <jan.bujanowski@gmail.com>
2022-12-05 23:35:09 +0000

    get ready day 6

---
+++ AdventOfCode2022/Day6.cs
@@ -0,0 +1,41 @@
+﻿using AdventOfCode.Shared;
+using AdventOfCode.Shared.Extensions;
+
+namespace AdventOfCode2022
+{
+
+    public class Day6 : Day66
+    {
+        string _strInput;
+        string[] _lines;
+
+        public override void ParseInput(string strInput)
+        {
+            _strInput = strInput;
+            _lines = _strInput.Split("\r\n");
+        }
+
+        private int FindStartingIndex(string[] lines)
+        {
+            int i = 0;
+           
+            return i;
+        }
+
+       
+        public override object StarOne()
+        {
+            int sum = 0;
+
+            return sum;
+        }
+
+       
+        public override object StarTwo()
+        {
+            int sum = 0;
+
+            return sum;
+        }
+    }
+}
