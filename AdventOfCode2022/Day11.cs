532d1b45aa526ce364f6b12bd3ba4ebe4ca59476
janbujanowski <jan.bujanowski@gmail.com>
2022-12-12 01:39:13 +0100

    day 11 star one

---
+++ AdventOfCode2022/Day11.cs
@@ -0,0 +1,116 @@
+﻿using AdventOfCode.Shared;
+using AdventOfCode.Shared.Extensions;
+using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Linq;
+using System.Net.Http.Headers;
+using System.Numerics;
+
+namespace AdventOfCode2022
+{
+    class Monkey
+    {
+        public long Id;
+        public long DivisibleBy;
+        public Queue<long> Items;
+        public long TrueThrowTo;
+        public long FalseThrowTo;
+        public Func<long, long> Operation;
+        public long Inspections;
+    }
+
+    public class Day11 : Day66
+    {
+        string _strInput;
+        List<Monkey> _monkeys;
+
+        public override void ParseInput(string strInput)
+        {
+            _monkeys = new List<Monkey>();
+            _strInput = strInput;
+            var lines = _strInput.SplitWithEnter();
+            for (long i = 0; i < lines.Length; i += 7)
+            {
+                long monkeyId = long.Parse(lines[i].Split(' ')[1].Split(':')[0]);
+                Queue<long> items = new Queue<long>(lines[i + 1].Split(':')[1].Trim().Split(',').Select(item => long.Parse(item)));
+                Func<long, long> func = GetFunction(lines[i + 2]);
+                long divisibleBy = long.Parse(lines[i + 3].Trim().Split(' ')[3]);
+                long trueThrowTo = long.Parse(lines[i + 4].Trim().Split(' ')[5]);
+                long falseThrowTo = long.Parse(lines[i + 5].Trim().Split(' ')[5]);
+
+                var monkey = new Monkey()
+                {
+                    Id = monkeyId,
+                    Items = items,
+                    Operation = func,
+                    DivisibleBy = divisibleBy,
+                    TrueThrowTo = trueThrowTo,
+                    FalseThrowTo = falseThrowTo,
+                    Inspections = 0
+                };
+                _monkeys.Add(monkey);
+            }
+        }
+
+        private Func<long, long> GetFunction(string v)
+        {
+            var longerestingPart = v.Split('=')[1].Trim().Split(' ');
+            var isMultply = longerestingPart[1][0] == '*';
+            var isOld = longerestingPart[2] == "old";
+            if (isOld)
+            {
+                return (x) => { return x * x; };
+            }
+            var numberPart = long.Parse(longerestingPart[2]);
+            if (isMultply)
+            {
+                return (x) => { return x * numberPart; };
+            }
+            return (x) => { return x + numberPart; };
+
+        }
+
+        private void Round(List<Monkey> monkeys)
+        {
+            foreach (var monkey in monkeys)
+            {
+                while (monkey.Items.Count > 0)
+                {
+                    var item = monkey.Items.Dequeue();
+                    var newitemworryLevel = monkey.Operation(item);
+                    monkey.Inspections++;
+                    if (newitemworryLevel % monkey.DivisibleBy == 0)
+                    {
+                        monkeys.Find(monkeyThrow => monkeyThrow.Id== monkey.TrueThrowTo).Items.Enqueue(newitemworryLevel);
+                    }
+                    else
+                    {
+                        monkeys.Find(monkeyThrow => monkeyThrow.Id == monkey.FalseThrowTo).Items.Enqueue(newitemworryLevel);
+                    }
+                }
+            }
+        }
+        public override object StarOne()
+        {
+            for (long i = 0; i < 20; i++)
+            {
+                Round(_monkeys);
+            }
+            var oredered = _monkeys.OrderByDescending(x => x.Inspections);
+            return oredered.ElementAt(0).Inspections * oredered.ElementAt(1).Inspections;
+        }
+        
+        public override object StarTwo()
+        {
+            ParseInput(_strInput);
+            for (long i = 0; i < 10000; i++)
+            {
+                Round(_monkeys);
+            }
+            var oredered = _monkeys.OrderByDescending(x => x.Inspections);
+            var first = oredered.ElementAt(0).Inspections;
+            return oredered.ElementAt(0).Inspections * oredered.ElementAt(1).Inspections;
+        }
+    }
+}
